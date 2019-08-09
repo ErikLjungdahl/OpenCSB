c97683bc68b73f3411ee1442c6d4ba5579dfca91
ErikLjungdahl <ekke.ekke@gmail.com>
2019-08-09 23:42:48 +0200

    -||-

---
+++ app/src/main/java/com/example/opencsb/SettingsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.opencsb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportFragmentManager.beginTransaction()
+            .replace(android.R.id.content, SettingsFragment())
+            .commit()
+    }
+}
---
+++ app/src/main/java/com/example/opencsb/SettingsFragment.kt
@@ -0,0 +1,25 @@
+package com.example.opencsb
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.preference.PreferenceFragmentCompat
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+}
