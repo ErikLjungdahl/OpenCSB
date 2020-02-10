ef10fb7d59fc787bae03856303288554a283df51
ErikLjungdahl <ekke.ekke@gmail.com>
2020-02-10 11:38:02 +0100

    1.0

--- app/src/main/java/erikljungdahl/opencsb/SettingsFragment.kt
+++ app/src/main/java/erikljungdahl/opencsb/SettingsFragment.kt
@@ -4,7 +4,10 @@ package erikljungdahl.opencsb
 import android.content.Intent
 import android.content.pm.ShortcutInfo
 import android.content.pm.ShortcutManager
+import android.graphics.drawable.Icon
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.preference.EditTextPreference
 import androidx.preference.EditTextPreference.SimpleSummaryProvider.getInstance
 import androidx.preference.Preference
@@ -15,24 +18,26 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
-
         val pref_doorID : EditTextPreference? = findPreference("doorID")
         pref_doorID?.summaryProvider = getInstance()
-        pref_doorID?.setOnPreferenceChangeListener { preference: Preference, newValue: Any ->
+        pref_doorID?.setOnPreferenceChangeListener { _: Preference, newValue: Any ->
             createShortcut(newValue.toString())
             true
         }
+
+        val feedback : Preference? = findPreference("feedback")
+        feedback?.setOnPreferenceClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/ErikLjungdahl/OpenCSB/issues")))
+            true
+        }
     }
- /*   fun sendFeedback (view: View) {
-        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/ErikLjungdahl/OpenCSB/issues")))
-    }
-*/
 
     fun createShortcut (doorID : String) {
         val shortcutManager: ShortcutManager = activity!!.getSystemService(ShortcutManager::class.java)!!
         val shortcut = ShortcutInfo.Builder(activity, "doorID")
             .setShortLabel("AutoOpenCSB")
             .setLongLabel("Automatically OpenCSB")
+            .setIcon(Icon.createWithResource(context, R.mipmap.ic_shortcut_autoopencsb))
             .setIntent(
                 Intent(activity!!.applicationContext, MainActivity::class.java)
                     .setAction(Intent.ACTION_MAIN)
